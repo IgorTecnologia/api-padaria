a4706f4f24dc9d39cb3896f5ed5416df7c9fd517
IgorTecnologia <iguigu2019@gmail.com>
2024-07-06 20:50:52 -0300

    feat: User Repository

---
+++ api-padaria/src/main/java/br/com/empresa/padaria/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package br.com.empresa.padaria.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import br.com.empresa.padaria.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	public Page<User> findAllByFirstNameContainingIgnoreCase(@Param ("firstName") String firstName, Pageable pageable);
+}
