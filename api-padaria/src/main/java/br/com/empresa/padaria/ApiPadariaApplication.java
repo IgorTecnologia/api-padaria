bc3d22e0360f7323ec7f1d86b0040e260311e7dd
IgorTecnologia <iguigu2019@gmail.com>
2024-07-06 21:00:07 -0300

    feat: Api Padaria Application

---
+++ api-padaria/src/main/java/br/com/empresa/padaria/ApiPadariaApplication.java
@@ -0,0 +1,14 @@
+package br.com.empresa.padaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPadariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPadariaApplication.class, args);
+	}
+
+}
+
