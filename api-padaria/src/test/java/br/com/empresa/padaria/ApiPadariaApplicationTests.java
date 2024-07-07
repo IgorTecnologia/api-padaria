465a04846c0517a0117a742067145e8bf6c5ea25
IgorTecnologia <iguigu2019@gmail.com>
2024-07-06 21:03:40 -0300

    feat: Api Padaria Application Tests

---
+++ api-padaria/src/test/java/br/com/empresa/padaria/ApiPadariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresa.padaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPadariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
