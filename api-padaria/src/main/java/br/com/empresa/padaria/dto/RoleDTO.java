11d2b3417233f0ba89c48a08899248d412333715
IgorTecnologia <iguigu2019@gmail.com>
2024-12-15 02:04:20 -0300

    Role DTO

--- api-padaria/src/main/java/br/com/empresa/padaria/dto/RoleDTO.java
+++ api-padaria/src/main/java/br/com/empresa/padaria/dto/RoleDTO.java
@@ -1,16 +1,29 @@
 package br.com.empresa.padaria.dto;
 
 import br.com.empresa.padaria.entities.Role;
+import br.com.empresa.padaria.validations.AuthorityConstraint;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
 public class RoleDTO {
 
-	private Long id;
+	private UUID id;
+
+	@NotNull(message = "The authority field is mandatory.")
+	@Size(min = 2, max = 30, message = "Minimum characters allowed are 2 e maximum are 30.")
+	@AuthorityConstraint(message = "The authority already exists (not allowed).")
 	private String authority;
 	
 	public RoleDTO() {
 	}
 
-	public RoleDTO(Long id, String authority) {
+	public RoleDTO(UUID id, String authority) {
 		this.id = id;
 		this.authority = authority;
 	}
@@ -20,21 +33,4 @@ public class RoleDTO {
 		id = entity.getId();
 		authority = entity.getAuthority();
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getAuthority() {
-		return authority;
-	}
-
-	public void setAuthority(String authority) {
-		this.authority = authority;
-	}
-
 }
