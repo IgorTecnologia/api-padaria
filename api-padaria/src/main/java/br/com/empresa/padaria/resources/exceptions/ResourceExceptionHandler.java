90b367fa2fc71445c1627321b9e5840eaef751bb
IgorTecnologia <iguigu2019@gmail.com>
2024-12-15 02:10:44 -0300

    Resource Exception Handler

--- api-padaria/src/main/java/br/com/empresa/padaria/resources/exceptions/ResourceExceptionHandler.java
+++ api-padaria/src/main/java/br/com/empresa/padaria/resources/exceptions/ResourceExceptionHandler.java
@@ -1,13 +1,16 @@
 package br.com.empresa.padaria.resources.exceptions;
 
-import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import br.com.empresa.padaria.services.exceptions.DataBaseException;
 import br.com.empresa.padaria.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -20,8 +23,8 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError();
 		
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		
-		err.setTimeStamp(Instant.now());
+
+		err.setTimeStamp(LocalDateTime.now(ZoneId.of("UTC")));
 		err.setStatus(status.value());
 		err.setError("Entity not found");
 		err.setMessage(e.getMessage());
@@ -30,19 +33,40 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 	
-	@ExceptionHandler(DataBaseException.class)
-	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandardError> dataBase(DataIntegrityViolationException e, HttpServletRequest request){
 		
 		StandardError err = new StandardError();
 		
 		HttpStatus status =  HttpStatus.BAD_REQUEST;
-		
-		err.setTimeStamp(Instant.now());
+
+		err.setTimeStamp(LocalDateTime.now(ZoneId.of("UTC")));
 		err.setStatus(status.value());
 		err.setError("DataBase Exception");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
 		
 		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidatorError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+
+		ValidatorError err =  new ValidatorError();
+
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+		err.setTimeStamp(LocalDateTime.now(ZoneId.of("UTC")));
+		err.setStatus(status.value());
+		err.setError("Validation Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		for(FieldError f : e.getBindingResult().getFieldErrors()){
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(status).body(err);
+
 	}
 }
