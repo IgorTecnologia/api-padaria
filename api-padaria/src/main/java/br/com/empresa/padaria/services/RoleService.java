9d2054f1fd2100641bd28680735e63b6133f48f1
IgorTecnologia <iguigu2019@gmail.com>
2024-12-15 02:04:51 -0300

    Role Service

--- api-padaria/src/main/java/br/com/empresa/padaria/services/RoleService.java
+++ api-padaria/src/main/java/br/com/empresa/padaria/services/RoleService.java
@@ -1,83 +1,23 @@
 package br.com.empresa.padaria.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
+import br.com.empresa.padaria.dto.RoleDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import br.com.empresa.padaria.dto.RoleDTO;
-import br.com.empresa.padaria.entities.Role;
-import br.com.empresa.padaria.repositories.RoleRepository;
-import br.com.empresa.padaria.services.exceptions.DataBaseException;
-import br.com.empresa.padaria.services.exceptions.ResourceNotFoundException;
+import java.util.List;
+import java.util.UUID;
 
-@Service
-public class RoleService {
+public interface RoleService {
 
-	@Autowired
-	private RoleRepository repository;
+    Page<RoleDTO> findAllPaged(Pageable pageable);
 
-	@Transactional(readOnly = true)
-	public Page<RoleDTO> findAllPaged(Pageable pageable) {
+    Page<RoleDTO> queryMethod(String authority, Pageable pageable);
 
-		Page<Role> page = repository.findAll(pageable);
-		return page.map(x -> new RoleDTO(x));
-	}
+    RoleDTO findById(UUID id);
 
-	@Transactional(readOnly = true)
-	public Page<RoleDTO> queryMethod(String authority, Pageable pageable){
-		
-		Page<Role> page = repository.findAllByAuthorityContainingIgnoreCase(authority, pageable);
-		return page.map(x -> new RoleDTO(x));
-	}
-	
-	@Transactional(readOnly = true)
-	public RoleDTO findById(Long id) {
+    RoleDTO insert(RoleDTO dto);
 
-		Optional<Role> obj = repository.findById(id);
-		Role entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + id));
-		return new RoleDTO(entity);
-	}
+    RoleDTO update(UUID id, RoleDTO dto);
 
-	@Transactional
-	public RoleDTO insert(RoleDTO dto) {
-
-		Role entity = new Role();
-		copyDtoToEntity(entity, dto);
-		repository.save(entity);
-		return new RoleDTO(entity);
-	}
-
-	@Transactional
-	public RoleDTO update(Long id, RoleDTO dto) {
-
-		Optional<Role> obj = repository.findById(id);
-		Role entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + id));
-		entity.setAuthority(dto.getAuthority());
-		repository.save(entity);
-		return new RoleDTO(entity);
-	}
-
-	public void deleteById(Long id) {
-		try {
-			Optional<Role> obj = repository.findById(id);
-
-			if (obj.isEmpty()) {
-				throw new ResourceNotFoundException("Id not found: " + id);
-			}
-
-			repository.deleteById(id);
-		} catch (DataIntegrityViolationException e) {
-			throw new DataBaseException("Integrity violation");
-		}
-	}
-
-	public void copyDtoToEntity(Role entity, RoleDTO dto) {
-
-		entity.setAuthority(dto.getAuthority());
-	}
+    void deleteById(UUID id);
 }
