69db5f9d7fd4bc329ab555705127a262561896ff
IgorTecnologia <iguigu2019@gmail.com>
2024-12-15 02:07:19 -0300

    Authority Constraint

---
+++ api-padaria/src/main/java/br/com/empresa/padaria/validations/AuthorityConstraint.java
@@ -0,0 +1,21 @@
+package br.com.empresa.padaria.validations;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = AuthorityConstraintImpl.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AuthorityConstraint {
+
+    String message() default "";
+    Class<?> [] groups() default {};
+    Class<? extends Payload> [] payload() default {};
+}
