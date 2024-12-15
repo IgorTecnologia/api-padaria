40309379981361e897bf8fd08036e9575fae59f3
IgorTecnologia <iguigu2019@gmail.com>
2024-12-15 02:25:22 -0300

    User Repository

--- api-padaria/src/main/java/br/com/empresa/padaria/repositories/UserRepository.java
+++ api-padaria/src/main/java/br/com/empresa/padaria/repositories/UserRepository.java
@@ -3,14 +3,13 @@ package br.com.empresa.padaria.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
 
 import br.com.empresa.padaria.entities.User;
 
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
+public interface UserRepository extends JpaRepository<User, UUID>{
 
     Page<User> findAllByFirstNameContainingIgnoreCase(@Param ("firstName") String firstName, Pageable pageable);
 
