438a0e2cfbbf74bc7316e301ec4046049229df6b
IgorTecnologia <iguigu2019@gmail.com>
2024-12-15 02:13:42 -0300

    Role Repository Tests

--- api-padaria/src/test/java/br/com/empresa/padaria/repositories/RoleRepositoryTests.java
+++ api-padaria/src/test/java/br/com/empresa/padaria/repositories/RoleRepositoryTests.java
@@ -1,17 +1,20 @@
 package br.com.empresa.padaria.repositories;
 
 import java.util.Optional;
+import java.util.UUID;
 
+import br.com.empresa.padaria.entities.Category;
+import br.com.empresa.padaria.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import br.com.empresa.padaria.dto.RoleDTO;
 import br.com.empresa.padaria.entities.Role;
 import br.com.empresa.padaria.tests.Factory;
 
@@ -20,16 +23,12 @@ public class RoleRepositoryTests {
 
 	@Autowired
 	private RoleRepository repository;
-	
-	private Long existingId;
-	private Long nonExistingId;
+
 	private Long countTotalElements;
 	
 	@BeforeEach
 	void setUp() throws Exception{
-		
-		existingId = 1L;
-		nonExistingId = 4L;
+
 		countTotalElements = 3L;
 	}
 	
@@ -46,18 +45,14 @@ public class RoleRepositoryTests {
 	
 	@Test
 	public void findByIdShouldReturnObjectWhenIdExisting() {
-		
-		Optional<Role> obj = repository.findById(existingId);
-		
-		Assertions.assertTrue(obj.isPresent());
-	}
-	
-	@Test
-	public void findByIdShouldReturnEmptyOptinalWhenIdNonExisting() {
-		
-		Optional<Role> obj = repository.findById(nonExistingId);
-		
-		Assertions.assertFalse(obj.isPresent());
+
+		Optional<Role> obj = repository.findAll().stream().findFirst();
+		UUID id = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+		Optional<Role> optional = repository.findById(id);
+
+		Assertions.assertNotNull(optional);
+		Assertions.assertTrue(optional.isPresent());
 	}
 	
 	@Test
@@ -66,32 +61,20 @@ public class RoleRepositoryTests {
 		Role entity = Factory.createdRole();
 		
 		repository.save(entity);
-		
+
+		Assertions.assertNotNull(entity);
 		Assertions.assertEquals(countTotalElements + 1, repository.count());
 	}
 	
 	@Test
-	public void saveShouldUpdateObjectWhenCorrectStructure() {
-		
-		RoleDTO dto = Factory.createdRoleDTO();
-		Role entity = Factory.createdRole();
-		
-		entity.setId(dto.getId());
-		entity.setAuthority(dto.getAuthority());
-		
-		repository.save(entity);
-		
-		Assertions.assertEquals(countTotalElements, repository.count());
-		Assertions.assertEquals(1L, entity.getId());
-	}
-	
-	@Test
 	public void deleteByIdShouldDeleteObjectWhenIdExisting() {
-		
-		repository.deleteById(existingId);
-		
-		Optional<Role> obj = repository.findById(existingId);
-		
-		Assertions.assertTrue(obj.isEmpty());
+
+		Optional<Role> obj = repository.findAll().stream().findFirst();
+		UUID id = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+		Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+			repository.deleteById(id);
+			throw new DataIntegrityViolationException("Integrity Violation");
+		});
 	}
 }
