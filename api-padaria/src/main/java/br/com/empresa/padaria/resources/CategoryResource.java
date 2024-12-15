6cb81cba33675b5a5fdc23b638819b9ead674105
IgorTecnologia <iguigu2019@gmail.com>
2024-12-15 02:01:32 -0300

    Category Resource

--- api-padaria/src/main/java/br/com/empresa/padaria/resources/CategoryResource.java
+++ api-padaria/src/main/java/br/com/empresa/padaria/resources/CategoryResource.java
@@ -1,5 +1,6 @@
 package br.com.empresa.padaria.resources;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,54 +15,64 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.empresa.padaria.dto.CategoryDTO;
-import br.com.empresa.padaria.services.CategoryService;
+import br.com.empresa.padaria.services.impl.CategoryServiceImpl;
+
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@Autowired
-	private CategoryService service;
+	private CategoryServiceImpl service;
 	
 	@GetMapping
 	public ResponseEntity<Page<CategoryDTO>> findAllPaged(Pageable pageable){
 		
 		Page<CategoryDTO> page = service.findAllPaged(pageable);
+		if(!page.isEmpty()){
+			for(CategoryDTO dto : page.toList()){
+				dto.add(linkTo(methodOn(CategoryResource.class).findById(dto.getId())).withSelfRel());
+			}
+		}
 		return ResponseEntity.ok().body(page);
 	}
 	
 	@GetMapping(value = "/name/{name}")
 	public ResponseEntity<Page<CategoryDTO>> queryMethod(Pageable pageable, @PathVariable String name){
 		
-		Page<CategoryDTO> page = service.queryMethod(pageable, name);
+		Page<CategoryDTO> page = service.queryMethod(name, pageable);
 		return ResponseEntity.ok().body(page);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+	public ResponseEntity<CategoryDTO> findById(@PathVariable UUID id){
 		
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
 	
 	@PostMapping
-	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+	public ResponseEntity<CategoryDTO> insert(@RequestBody @Valid CategoryDTO dto){
 		
 		dto = service.insert(dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto){
+	public ResponseEntity<CategoryDTO> update(@PathVariable UUID id, @RequestBody @Valid CategoryDTO dto){
 		
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> deleteById(@PathVariable Long id){
+	public ResponseEntity<Object> deleteById(@PathVariable UUID id){
 		
 		service.deleteById(id);
-		return ResponseEntity.noContent().build();
+		return ResponseEntity.ok().body("Category deleted successfully.");
 	}
 }
