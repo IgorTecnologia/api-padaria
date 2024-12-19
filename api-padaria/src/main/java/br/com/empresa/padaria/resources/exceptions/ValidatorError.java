7f0f67343e93575043825d95e7d19a8baafb3fcd
IgorTecnologia <iguigu2019@gmail.com>
2024-12-19 06:26:26 -0300

    Validator Error

--- api-padaria/src/main/java/br/com/empresa/padaria/resources/exceptions/ValidatorError.java
+++ api-padaria/src/main/java/br/com/empresa/padaria/resources/exceptions/ValidatorError.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Data
 public class ValidatorError extends StandardError{
@@ -12,5 +13,19 @@ public class ValidatorError extends StandardError{
 
     public void addError(String fieldName, String message){
         errors.add(new FieldMessage(fieldName, message));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ValidatorError that = (ValidatorError) o;
+        return Objects.equals(errors, that.errors);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), errors);
     }
 }
